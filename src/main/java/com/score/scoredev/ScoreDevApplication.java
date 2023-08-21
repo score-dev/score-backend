881839d63c472c4d0b6d63a0dbb26157fdfa2ccc
making-a-scene <4936joo@naver.com>
2023-08-21 11:38:12 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/score/scoredev/ScoreDevApplication.java
@@ -0,0 +1,13 @@
+package com.score.scoredev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoreDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoreDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/score/scoredev/ScoreDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.score.scoredev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
