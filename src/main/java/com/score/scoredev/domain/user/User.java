19a47344b360b53caaa96cd21a7216ac472c22eb
making-a-scene <4936joo@naver.com>
2023-08-22 14:04:17 +0900

    feat: User 클래스 구현

---
+++ src/main/java/com/score/scoredev/domain/user/User.java
@@ -0,0 +1,27 @@
+package com.score.scoredev.domain.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+@Getter
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "grade")
+    private int grade;
+
+    @Column(name = "ability_degree")
+    private int abilityDegree;
+
+    @Column(name = "join_date")
+    private Date joinDate = new Date();
+}
