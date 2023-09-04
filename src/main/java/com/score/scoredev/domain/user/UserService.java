d005fb4c640a888a6e0ca765ac8a5f7ff2b7a057
making-a-scene <4936joo@naver.com>
2023-09-04 13:41:17 +0900

    feat: UserService 클래스에 회원가입을 위한 join() 메소드 추가

--- src/main/java/com/score/scoredev/domain/user/UserService.java
+++ src/main/java/com/score/scoredev/domain/user/UserService.java
@@ -1,2 +1,17 @@
-package com.score.scoredev.domain.user;public class UserService {
+package com.score.scoredev.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void join(User user) {
+        userRepository.save(user);
+    }
 }
