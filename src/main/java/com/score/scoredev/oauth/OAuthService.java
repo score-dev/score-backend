6fc2334b330a3b7e7796fe3b84f0570f8fd7b089
making-a-scene <4936joo@naver.com>
2023-09-09 15:38:25 +0900

    refactor: 불필요한 import문 삭제

--- src/main/java/com/score/scoredev/oauth/OAuthService.java
+++ src/main/java/com/score/scoredev/oauth/OAuthService.java
@@ -1,24 +1,12 @@
 package com.score.scoredev.oauth;
 
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import net.minidev.json.parser.ParseException;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Service
 public class OAuthService {
