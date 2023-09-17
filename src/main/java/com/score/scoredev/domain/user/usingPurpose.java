058076c8f59e98aa39f600b2eb85a61f06d62ca0
making-a-scene <4936joo@naver.com>
2023-09-17 13:38:04 +0900

    feat: 스코어 사용 목적 저장을 위한 enum 생성

---
+++ src/main/java/com/score/scoredev/domain/user/usingPurpose.java
@@ -0,0 +1,17 @@
+package com.score.scoredev.domain.user;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum usingPurpose {
+    STRENGTH, // 기초체력
+    EXERCISE_HABIT, // 운동 습관 형성
+    HEALTH, // 헬스
+    EVENT_RECOMMEND, // 운동 종목 추천
+    TOGETHER, // 친구들과 함께 운동하기
+    DIET, // 다이어트
+    EATING_HABIT, // 식단 조절
+    SCHOOL_MENU, // 학교 급식표
+    ENTRANCE_EXAM // 입시
+
+}
