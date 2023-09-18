30782141057721fa4e0e82b8b1533909df2222fa
making-a-scene <4936joo@naver.com>
2023-09-18 14:46:08 +0900

    refactor: 전체적인 리팩토링

--- src/main/java/com/score/scoredev/domain/user/UserController.java
+++ src/main/java/com/score/scoredev/domain/user/UserController.java
@@ -1,20 +1,27 @@
 package com.score.scoredev.domain.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.http.HttpStatus.OK;
+
 @RestController
-@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
     @RequestMapping(value = "/user/auth/naver", method = RequestMethod.GET)
-    public String getNaverUserKey(@RequestParam("id") String userKey) {
-        return userKey;
+    public ResponseEntity<String> getNaverUserKey(@RequestParam("id") String userKey) {
+        System.out.println("test - " + userKey);
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(userKey, httpHeaders, OK);
     }
 
     @RequestMapping(value = "/user/auth/kakao", method = RequestMethod.GET)
@@ -26,5 +33,4 @@ public class UserController {
     public String getGoogleUserKey(@RequestParam("id") String userKey) {
         return userKey;
     }
-
 }
